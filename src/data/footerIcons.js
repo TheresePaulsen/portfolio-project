6096702d1ac8e7ca17892df5bb86b391eee623a2
Keke <keke.mokorosi@gmail.com>
2025-10-05 18:14:35 +0200

    footer and header merge

---
+++ src/data/footerIcons.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export const footerIcons = [
+    { iconName: "github", url: "https://github.com/theresepaulsen", ariaLabel: "GitHub" },
+    { iconName: "linkedin", url: "https://linkedin.com/in/theresepaulsen", ariaLabel: "LinkedIn" },
+    { iconName: "instagram", url: "https://www.instagram.com/theresepaulsen/", ariaLabel: "Instagram" },
+    { iconName: "facebook", url: "https://www.facebook.com/TheresePilborgPaulsen/", ariaLabel: "Facebook Profile" },
+]
