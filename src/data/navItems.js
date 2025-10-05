5d6a5a3a0267264b9dd5ab9f6cbd0ec5bc7683d2
Keke <keke.mokorosi@gmail.com>
2025-10-05 13:32:04 +0200

    nav component

---
+++ src/data/navItems.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export const navItems = [
+    { title: "About Me", url: "about.html" },
+    { title: "Case Studies", url: "case-studies.html" },
+    { title: "Competences", url: "competences.html" },
+    { title: "Contact", url: "contact.html" },
+]
---
+++ src/ui-components/Navbar.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import { navItems } from "../data/navItems";
+
+export function createNavbar() {
+    return `
+    <a href="index.html">
+          <img class="logo-img" src="img/logo3.png" alt="logo" />
+        </a>
+    <nav>
+          <ul>
+          ${navItems
+            .map(
+              (item) =>
+                `<li>
+                <a href="${item.url}">${item.title}</a>
+              </li>`
+            )
+            .join("")}
+          </ul>
+        </nav>`;
+}
